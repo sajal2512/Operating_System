d991317e30c4503dc7353d97c7f8413abcb3464c
sajal2512 <shrivastavasajal77@gmail.com>
2026-04-06 23:23:20 +0530

    Create pract3substraction.c

---
+++ pract3substraction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sub;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    
+    sub = a - b;
+    
+    printf("Subtraction = %d\n", sub);
+    
+    return 0;
+}
