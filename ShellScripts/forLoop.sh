cc33a5737e308a6f419c194c4b63eb2e7224a313
sajal2512 <shrivastavasajal77@gmail.com>
2026-04-20 20:50:54 +0530

    Add for loop examples in forLoop.sh

---
+++ ShellScripts/forLoop.sh
@@ -0,0 +1,17 @@
+for i in 1 2 3
+do
+echo "Number is $i"
+done
+echo
+echo "Second Method"
+
+for j in Symbiosis Institute of Technology Nagpur
+do
+echo "Institute Name is $j"
+done
+echo
+echo "Third"
+for x in {1..5}
+do
+echo "Number is $x"
+done
